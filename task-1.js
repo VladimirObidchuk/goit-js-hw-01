11e3507505d127dd160071bdc36b0535b37614d9
Vladimir <kremench@gmail.com>
2024-12-21 19:09:40 +0200

    adds all files and first task

---
+++ task-1.js
@@ -0,0 +1,5 @@
+function makeTransaction(quantity, pricePerDroid) {
+  const result = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  console.log(makeTransaction(5, 3000));
+  return result;
+}
